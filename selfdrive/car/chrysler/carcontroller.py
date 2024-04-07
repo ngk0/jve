2158e085f3579e06a795a1b175d38bef9219a9ce
John <4vanetten@gmail.com>
2024-04-06 18:18:00 -0600

    derp v3

--- selfdrive/car/chrysler/carcontroller.py
+++ selfdrive/car/chrysler/carcontroller.py
@@ -127,7 +127,7 @@ class CarController:
       else:
         apply_steer = apply_meas_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorqueEps, self.params)
 
-      if not lkas_active or not lkas_control_bit:
+      if not CC.latActive or not lkas_control_bit:
         apply_steer = 0
 
       self.apply_steer_last = apply_steer
