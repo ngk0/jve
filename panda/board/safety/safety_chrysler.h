66cb062decdd4a025cda1a482987310a807692f2
John <4vanetten@gmail.com>
2024-04-12 10:35:16 -0600

    wait

--- panda/board/safety/safety_chrysler.h
+++ panda/board/safety/safety_chrysler.h
@@ -188,7 +188,7 @@ static void chrysler_rx_hook(const CANPacket_t *to_push) {
   // enter controls on rising edge of ACC, exit controls on ACC off
   const int das_3_bus = (chrysler_platform == CHRYSLER_PACIFICA) ? 0 : 2;
   if ((bus == das_3_bus) && (addr == chrysler_addrs->DAS_3)) {
-    bool cruise_engaged = true || GET_BIT(to_push, 20U);
+    bool cruise_engaged = GET_BIT(to_push, 20U);
     pcm_cruise_check(cruise_engaged);
   }
 
